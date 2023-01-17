c3b0f79b9e78f018fc58b0dc09d69fe20c132d30
Sajjadul Islam Shuvo <100591443+sajjadul-islam-shuvo@users.noreply.github.com>
2023-01-17 20:20:02 +0600

    Add files via upload

---
+++ java-date-and-time.java
@@ -0,0 +1,16 @@
+import java.util.Calendar;
+import java.util.Scanner;
+public class practice {
+    public static String weekDay(int m, int d, int y) {
+        Calendar c = Calendar.getInstance();
+        c.set(y,m-1,d);
+        int w = c.get(Calendar.DAY_OF_WEEK);
+        String[] day = {"SUNDAY","MONDAY","TUESDAY","WEDNESDAY","THURSDAY","FRIDAY","SATURDAY"};
+        return day[w-1];
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt(), d=sc.nextInt(), y=sc.nextInt();
+        System.out.println(weekDay(m,d,y));
+    }
+}
