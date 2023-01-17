a0031ed939c1fcfa0234384b1efe4e8f3eef6deb
Sajjadul Islam Shuvo <100591443+sajjadul-islam-shuvo@users.noreply.github.com>
2023-01-17 20:42:03 +0600

    Create java-data-types.java

---
+++ java-data-types.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class hackerrank {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T != 0) {
+            T--;
+            try {
+                long n = sc.nextLong();
+                System.out.println(n + " can be fitted in:");
+                if (n >= Byte.MIN_VALUE && n <= Byte.MAX_VALUE) {
+                    System.out.println("* byte");
+                }
+                if (n >= Short.MIN_VALUE && n <= Short.MAX_VALUE) {
+                    System.out.println("* short");
+                }
+                if (n >= Integer.MIN_VALUE && n <= Integer.MAX_VALUE) {
+                    System.out.println("* int");
+                }
+                if (n >= Long.MIN_VALUE && n <= Long.MAX_VALUE) {
+                    System.out.println("* long");
+                }
+            } catch (Exception e) {
+                System.out.println(sc.next() + " can't be fitted anywhere.");
+            }
+        }
+    }
+}
