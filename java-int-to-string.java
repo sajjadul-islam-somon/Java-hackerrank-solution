68547fa244ea9691aa7bc1a4af4b8038203c0d67
Sajjadul Islam Shuvo <100591443+sajjadul-islam-shuvo@users.noreply.github.com>
2023-01-17 20:44:01 +0600

    Create java-int-to-string.java

---
+++ java-int-to-string.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class solve3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        if (a>=-100 && a<=100) {
+            System.out.println("Good job");
+        } else {
+            System.out.println("Wrong answer");
+        }
+    }
+}
